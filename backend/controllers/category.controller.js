b771ed39da7e2f5fdae31476f9f24c615caa02b4
faddy <faddybenhassin@gmail.com>
2026-07-18 19:14:13 +0100

    bored

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,3 +1,6 @@
+import { Category } from "../models/category.module.js";
+import { Product } from "../models/product.module.js";
+
 function buildCategoryTree(categories) {
     const map = {};
     const roots = [];
@@ -34,6 +37,21 @@ export async function getCategories(req, res) {
 }
 
 export async function getCategoryBySlug(req, res) {
+    try {
+        const { slug } = req.params;
+    
+        const category = await Category.findOne({ slug: slug }).lean();
+        if(!category){
+            return res.status(404).json({error: "Category not found"});
+        }
+    
+        return res.status(200).json(category);
+        
+    } catch (error) {
+        console.error("Error in getCategoryBySlug:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+
 }
 
 export async function getCategoryProducts(req, res) {
