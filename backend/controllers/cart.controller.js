e69d2e0a530badf5e0535ebde6e7e0bb477e5215
faddy <faddybenhassin@gmail.com>
2026-08-21 12:42:39 +0100

    skeleton

--- backend/controllers/cart.controller.js
+++ backend/controllers/cart.controller.js
@@ -1,7 +1,30 @@
+import Cart from '../models/cart.module.js'
+
+
 export async function getCart(req, res) {
     const cart = await Cart.findOne({ userId: req.session.user.id }).populate('items.variant')
     if(!cart){
         return res.status(404).json({ message: 'Cart not found'})
     }
     return res.status(200).json({cart, message: "cart found successfully"})
+}
+
+export async function updateCartItem(req,res ){
+
+}
+
+export async function removeFromCart(req,res ){
+    
+}
+
+export async function addToCart(req,res ){
+    
+}
+
+export async function clearCart(req,res ){
+    
+}
+
+export async function mergeCart(req,res ){
+    
 }
---
+++ backend/controllers/order.controller.js
@@ -0,0 +1,21 @@
+export async function createOrder(req,res ){
+
+}
+
+export async function getOrders(req,res ){
+
+}
+
+
+export async function getOrder(req,res ){
+
+}
+
+export async function updateOrderStatus(req,res ){
+
+}
+
+export async function cancelOrder(req,res ){
+
+}
+
